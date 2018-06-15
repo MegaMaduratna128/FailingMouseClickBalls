671c8c7812d498dc4ac114e68006421bd606e6c8
John Amos <jlajr36@gmail.com>
2018-06-15 07:36:09 -0400

    Add files via upload

---
+++ Assets/Scripts/NewBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBall : MonoBehaviour {
+	
+	public GameObject BallPrefab; 
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 worldpoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log(worldpoint);
+			Instantiate(BallPrefab,new Vector3(worldpoint.x, worldpoint.y,0),Quaternion.identity);
+		}
+	}
+}
